7f502ea26f0a69abe899bb014bb7f1c37b7a302f
Alex Faerber <alex@MacBookAir.lan>
2026-09-18 23:23:30 -0500

    Upload project files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,11 +7,15 @@ import AddResort from "./pages/AddResort";
 function App() {
   return (
     <BrowserRouter>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
-        <Link className="navbar-brand ms-4 nav-link" to="/">Home</Link>
-        <Link className="nav-link" to="/my-trip-lists">My Trip Lists</Link>
-        <Link className="nav-link" to="/add-resort">Add Resort</Link>
-      </nav>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+  <div className="container-fluid">
+    <Link className="navbar-brand ms-4" to="/">Home</Link>
+    <div className="navbar-nav flex-row gap-3">
+      <Link className="nav-link" to="/my-trip-lists">My Trip Lists</Link>
+      <Link className="nav-link" to="/add-resort">Add Resort</Link>
+    </div>
+  </div>
+</nav>
       <hr />
       <Routes>
         <Route path="/" element={<Home />} />
